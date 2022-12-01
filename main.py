05e1612efe578bd7299c32c70e8980ab0a292ea6
Victor <victormoralesblanco147@gmail.com>
2022-12-01 12:31:24 -0500

    update

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def divide_function(type_of_answer):
     # This line saves the first number in the list
     total = 1
     # This line initializes answer so that it may be referenced outside the if statement.
-    for item in range(list_of_numbers):
+    for item in range(len(list_of_numbers)):
         total = total * list_of_numbers[item]
     denominator = total / list_of_numbers[0]
     total = list_of_numbers[0] / denominator
